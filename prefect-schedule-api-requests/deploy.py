7fc49a04b4d712770ad234cbbcbd5c91cd730c8f
Ohad Chaet <ohadch9518@gmail.com>
2023-09-15 13:43:25 +0300

    fixes (#3)
    Co-authored-by: ohadchaet <ohad.chaet@lynx.md>

--- prefect-schedule-api-requests/deploy.py
+++ prefect-schedule-api-requests/deploy.py
@@ -4,7 +4,6 @@ from flows.suggest_activity import suggest_activity
 
 
 async def main():
-    # This line creates a deployment for the flow, which will be scheduled to run every day at 9:00 AM
     deployment = await Deployment.build_from_flow(
         name="Daily",
         flow=suggest_activity,
